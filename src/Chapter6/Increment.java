a26e2eb6de1110f67dc8df421832dd58af77e698
mystery <s3777242@rmit.edu.vn>
2020-06-11 21:56:36 +0700

    Add chapter 6

---
+++ src/Chapter6/Increment.java
@@ -0,0 +1,17 @@
+package Chapter6;
+
+public class Increment {
+    public static void main(String[] args) {
+
+        int x = 1;
+        System.out.println("Before the call, x is " + x);
+
+        increment(x);
+        System.out.println("After the call, x is " + x);
+    }
+    public static int increment(int n) {
+        n = n + 1;
+        System.out.println("x in the mothod is: "+n);
+        return n;
+    }
+}
